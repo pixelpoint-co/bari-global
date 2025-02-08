1d6abd1deb9363f074e0e2eed2d97c3ff256748a
eugene choi <eugene.maestro.choi@gmail.com>
2025-02-08 21:48:41 +0900

    add embedded loader to source index.html && try poster tag

--- dwo/src/components/TopPage.js
+++ dwo/src/components/TopPage.js
@@ -6,17 +6,16 @@ const TopPage = () => {
     <div className="relative w-full h-screen overflow-hidden flex items-center justify-center">
       {/* Background Video */}
       <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          className="absolute top-0 left-0 w-full h-full object-cover"
-        >
-          <source src="/DWO.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-
-
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        poster="https://images.squarespace-cdn.com/content/v1/5e10bdc20efb8f0d169f85f9/09943d85-b8c7-4d64-af31-1a27d1b76698/arrow.png?format=2500w"
+      >
+        <source src="/DWO.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       {/* PNG Image in Center (Bigger on Mobile) */}
       <img
         src="/DWO_middle.png"
